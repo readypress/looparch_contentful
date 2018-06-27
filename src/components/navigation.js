7a111a362ba729f2336dfa15619a19cea9b758b3
buttreygoodness <jeff@readypress.com>
2018-06-27 12:42:57 -0700

    Adding manufacturers

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -11,6 +11,9 @@ export default () => (
       <li className={styles.navigationItem}>
         <Link to="/blog/">Blog</Link>
       </li>
+      <li className={styles.navigationItem}>
+        <Link to="/manufacturers/">Manufacturers</Link>
+      </li>
     </ul>
   </nav>
 )
---
+++ src/pages/manufacturers/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+import ArticlePreview from '../../components/article-preview'
+
+class BlogIndex extends React.Component {
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const posts = get(this, 'props.data.allContentfulManufacturer.edges')
+
+    return (
+      <div style={{ background: '#fff' }}>
+        <Helmet title={siteTitle} />
+        <div className="wrapper">
+          <h2>Manufacturers</h2>
+          <ul className="article-list">
+            {posts.map(({ node }) => {
+              return (
+                <li key={node.slug}>
+                  <ArticlePreview article={node} />
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query ManufacturerIndexQuery {
+    allContentfulManufacturer(sort: { fields: [title], order: DESC }) {
+      edges {
+        node {
+          title
+          slug
+          publishDate(formatString: "MMMM Do, YYYY")
+          tags
+          heroImage {
+            file {
+              url
+            }
+          }
+          description {
+            childMarkdownRemark {
+              html
+            }
+          }
+        }
+      }
+    }
+  }
+`
