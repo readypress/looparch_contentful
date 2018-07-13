9c875a726710af4f44d16d45cfacc575823e17f0
buttreygoodness <jeff@readypress.com>
2018-07-13 12:37:31 -0700

    Adding contact form.

---
+++ src/components/contact-form.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+class ContactForm extends React.Component {
+  render() {
+    return (
+      <form name="contact" method="POST" data-netlify="true">
+        <div className="field">
+          <label className="label">Your Name:</label>
+          <div className="control">
+            <input className="input" type="text" name="name" autoComplete="name" required/>
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Your Email:</label>
+          <div className="control">
+            <input className="input" type="email" name="email" autoComplete="email" required/>
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Message:</label>
+          <div className="control">
+            <textarea className="textarea" name="message" />
+          </div>
+        </div>
+        <div className="field">
+          <div className="control">
+            <button type="submit" className="button is-link">Send</button>
+          </div>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default ContactForm
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,48 +3,31 @@ import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
 import GoogleMap from '../components/map'
+import ContactForm from '../components/contact-form'
 
 class ContactPage extends React.Component {
   render() {
     const siteTitle = this.props.data.site.siteMetadata.title
 
     return (
-      <div style={{ background: '#fff' }}>
+      <div>
         <Helmet title={`${siteTitle} | Contact Us`} />
-
         <GoogleMap />
-
         <section className="section">
           <div className="container">
-            <h2 className="title">Title</h2>
-            <p>
-              Dolore sit sit voluptate in eu tempor pariatur quis est excepteur
-              sint consectetur occaecat anim fugiat occaecat mollit occaecat
-              aute consequat ex aliqua aliqua in reprehenderit exercitation ad
-              non.
-            </p>
-          </div>
-        </section>
-        <section className="section">
-          <div className="container">
-            <h2 className="title">Title</h2>
-            <p>
-              Dolore sit sit voluptate in eu tempor pariatur quis est excepteur
-              sint consectetur occaecat anim fugiat occaecat mollit occaecat
-              aute consequat ex aliqua aliqua in reprehenderit exercitation ad
-              non.
-            </p>
-          </div>
-        </section>
-        <section className="section">
-          <div className="container">
-            <h2 className="title">Title</h2>
-            <p>
-              Dolore sit sit voluptate in eu tempor pariatur quis est excepteur
-              sint consectetur occaecat anim fugiat occaecat mollit occaecat
-              aute consequat ex aliqua aliqua in reprehenderit exercitation ad
-              non.
-            </p>
+            <h1 className="title">Contact Us</h1>
+            <div className="columns">
+              <div className="column is-two-thirds">
+                <ContactForm />
+              </div>
+              <div className="column">
+                <div className="content">
+                  <p>We’d love to hear from you. If you’re interested in receiving samples or talking to us about any of our product lines, feel free to fill out the form.</p>
+                  <p>Our mailing address is:</p>
+                  <p><strong>Loop Architectural Materials</strong><br/>3310 N 16th Street<br/>Phoenix, AZ 85016</p>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </div>
