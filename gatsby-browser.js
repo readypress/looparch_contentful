723c4c955a16dc3651082569cbbec1719347630e
buttreygoodness <jeff@readypress.com>
2019-07-02 17:59:40 -0700

    Service worker reloads.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,3 @@
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`
-  )
-
-  if (answer === true) {
-    window.location.reload()
-  }
+exports.onServiceWorkerUpdateReady = () => {
+  window.location.reload()
 }
